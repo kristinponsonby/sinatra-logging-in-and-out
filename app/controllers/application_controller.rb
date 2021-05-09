5c5d24284892551e43ab991459ebd4240e2016a7
Kristin Ponsonby <kristinponsonby@gmail.com>
2021-05-09 07:28:49 -0400

    commit tree 3db567446a9e92dd1fd8d5660e69692e1c42b3ad

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,22 +6,43 @@ class ApplicationController < Sinatra::Base
     set :session_secret, "secret"
   end
 
+    #renders index page with login form
   get '/' do
     erb :index
   end
 
+  #find the user in the database based on their username.
+  #If there is a match, set the session to the user's ID, redirect them to the /account route (using redirect to)
+  #use ERB to display the user's data on the page.
+  #If there is no match, render the error page.  
+
   post '/login' do
+    @user = User.find_by(:username => params[:username])
+    if @user != nil && @user.password == params[:password]
+      session[:user_id] = @user.id
+      redirect to '/account'
+    end
+    erb :error
+  end
 
   end
 
   get '/account' do
-
-  end
+    @current_user = User.find_by_id(session[:user_id])
+    if @current_user
+      erb :account
+    else
+      erb :error
+    end
+ 
 
   get '/logout' do
+    session.clear
+    redirect to '/'
 
   end
 
-
 end
+
+
 
--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,2 +1,14 @@
+require 'sinatra/base'
 class Helpers
+    #accept the session hash as an argument
+    #use the user_id from the session hash to find the user in the database and return that user.
+    def self.current_user(session)
+          @user = User.find_by_id(session[:user_id])
+    end
+      #accept the session hash as an argument
+      #This method should return true if the user_id is in the session hash and false if not.
+    def self.is_logged_in?(session)
+          !!session[:user_id]
+    end
+    
 end
